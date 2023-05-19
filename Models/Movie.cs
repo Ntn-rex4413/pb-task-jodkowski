541d8bc117d013388fd15293fcbe8ec612081fda
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-05-19 12:04:55 +0200

    Create Movie class

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalogAPI.Models
+{
+    public class Movie
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public  string? Genre { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+    }
+}
